06dd3af6e6e4833e8a63194c975dbc6ad0e294ca
Travis Collins <trc229@gmail.com>
2018-06-27 09:25:57 -0400

    Implement gif search field

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,17 @@
 //darksky-key = 402f7c1f1f1e88f16325562fad349ab9
 //gify-key = HcEkdzj7Hh7PCuX8qumUU5zJE06iNKTw
-var weatherIcon = [
-    "clear-day",
-    "clear-night",
-    "partly-cloudy-day",
-    "partly-cloudy-night",
-    "cloudy",
-    "rain",
-    "sleet",
-    "snow",
-    "wind",
-    "fog"
-], weatherBtn, rowNum, btnInRow, re, stillSrcArr = [], gifSrcArr = [], key = 'HcEkdzj7Hh7PCuX8qumUU5zJE06iNKTw';
+var weatherIcons = {
+    "Clear Day": '',
+    "Clear Night": '',
+    "Partly Cloudy Day": '',
+    "Partly Cloudy Night": '',
+    "Cloudy": '',
+    "Rain": '',
+    "Sleet": '',
+    "Snow": '',
+    "Wind": '',
+    "Fog": ''
+}, weatherBtn, rowNum, btnInRow, re, imgKey, query, stillSrcArr = [], gifSrcArr = [], apiKey = 'HcEkdzj7Hh7PCuX8qumUU5zJE06iNKTw';
 
 screenWidth = $(window).width();
 if (screenWidth < 500) {
@@ -24,21 +24,16 @@ if (screenWidth < 500) {
     btnInRow = 5;
 }
 
-for (var i = 0; i < weatherIcon.length; i++) {
-    rowNum = (i  - (i % btnInRow))/btnInRow;
-    weatherBtn = $('<button>').addClass('btn btn-secondary btn-font-size');
-    weatherBtn.text(prettify(weatherIcon[i]));
+var counter = 0;
+for (var key in weatherIcons) {
+    rowNum = (counter  - (counter % btnInRow))/btnInRow;
+    weatherBtn = $('<button>').addClass('btn btn-secondary btn-font-size weather-btn');
+    weatherBtn.text(key);
     $('#btn-row-' + rowNum).append(weatherBtn);
+    counter++;
 }
 
-// https://codereview.stackexchange.com/questions/87221/change-case-of-string-replace-hyphens-with-spaces-and-capitalize-first-letter
-function prettify(str) {
-    return str.split('-').map(function capitalize(part) {
-        return part.charAt(0).toUpperCase() + part.slice(1);
-    }).join(' ');
-}
-
-function toggleImg (img) {
+function selectedGif (img) {
     var imgNum = img.attr('id').match(/\d+/)[0];
     var imgType = img.attr('id').substr(0, img.attr('id').indexOf('-'));
 
@@ -51,15 +46,43 @@ function toggleImg (img) {
     }
 }
 
-$('.btn').on('click', function () {
+$('#submit-btn').on('click', function () {
+    query = $('#search-input').val();
+
+    $('#img-row').empty();
+    $.ajax({
+        url: 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + query + '&limit=10&offset=0&rating=G&lang=en',
+        method: 'GET' 
+    }).then(function (res) {
+        var stillImgSrc, gifSrc, stillImg;
+        stillSrcArr = [], gifSrcArr = [];
+
+        for (var i = 0; i < res.data.length; i++) {
+            stillImgSrc = res.data[i].images.fixed_height_small_still.url;
+            gifSrc = res.data[i].images.fixed_height_small.url;
+
+            stillSrcArr.push(stillImgSrc);
+            gifSrcArr.push(gifSrc);
+
+            stillImg = $('<img>').attr({'src': stillImgSrc, 'alt': query + ' Still Img', 'id':'still-img-' + [i]});
+            $('#img-row').append(stillImg);
+        }
+    });
+});
+
+$('.weather-btn').on('click', function () {
     $this = $(this);
     $this.blur();
-    var query = $this.text();
+    query = $this.text();
+
+    $('#search-copy').text('Choose your favorite ' + query.toLowerCase() + ' gif.');
+    $('#search-input').show();
+    $('#submit-btn').show();
 
     $('#img-row').empty();
 
     $.ajax({
-        url: 'https://api.giphy.com/v1/gifs/search?api_key=' + key + '&q=' + query + '&limit=10&offset=0&rating=G&lang=en',
+        url: 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + query + '&limit=10&offset=0&rating=G&lang=en',
         method: 'GET' 
     }).then(function (res) {
         var stillImgSrc, gifSrc, stillImg;
@@ -80,5 +103,11 @@ $('.btn').on('click', function () {
 
 $('#img-row').on('click', '[id*="-img-"]', function () {
     $this = $(this);
-    toggleImg($this);
+    selectedGif($this);
+
+    // Only 1 gif should be active at at time
 })
+
+// Add Submit button to assign gif to weather 
+// weatherIcons[query] = $this;
+// console.log(weatherIcons);
